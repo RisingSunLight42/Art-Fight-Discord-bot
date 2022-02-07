e6d1480bcbc3cf8fa386e910e59c6f01be73ec04
RisingSunLight <risingsunlight5776@gmail.com>
2022-02-07 10:39:01 +0100

    Corrections sur l'ajout des options

--- commands/initialisation.js
+++ commands/initialisation.js
@@ -6,15 +6,15 @@ const fs = require('fs'); // Permet d'écrire des fichiers avec Node.js
 // Crée la commande en faisant une nouvelle commande Slash
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('initalisation')
-        .setDescription('Initialise l\'Art Fight sur le serveur.')
+        .setName('initialisation')
+        .setDescription('Commande pour initialiser l\'Art Fight.')
         .addStringOption(option =>
-            option.setName("Équipe 1")
-                .setDescription("Nom de l'équipe 1 (veillez à ne pas donner un nom trop long !)")
+            option.setName('equipe1')
+                .setDescription('Nom de l\'Équipe 1, veillez à ne pas donner un nom trop long !')
                 .setRequired(true))
         .addStringOption(option =>
-            option.setName("Équipe 2")
-                .setDescription("Nom de l'équipe 2 (veillez à ne pas donner un nom trop long !)")
+            option.setName("equipe2")
+                .setDescription("Nom de l\'Équipe 2, veillez à ne pas donner un nom trop long !")
                 .setRequired(true)),
     /**
      * 
