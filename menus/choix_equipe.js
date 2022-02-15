526c4bf0be206ffe9c0810bd9f6f10eead8cb074
RisingSunLight <risingsunlight5776@gmail.com>
2022-02-15 19:40:44 +0100

    Ajout de la création de l'enregistrement de l'user

---
+++ menus/choix_equipe.js
@@ -0,0 +1,13 @@
+const { table_user } = require("../database/database_gestion.js"); // Import de la table pour l'artfight
+
+module.exports = {
+    name: "choix_equipe",
+    async execute(client, interaction) {
+        await table_user.create({
+            id_guild: interaction.guildId,
+            id_user: interaction.user.id,
+            nom_equipe: interaction.values[0],
+            points: 0,
+        });
+    },
+};
