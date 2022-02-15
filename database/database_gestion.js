cbb773a15ccbe2fad9a3b928ab74ac24c4a7bd5d
RisingSunLight <risingsunlight5776@gmail.com>
2022-02-15 10:59:36 +0100

    Ajout de la table_user

--- database/database_gestion.js
+++ database/database_gestion.js
@@ -1,17 +1,18 @@
-const Sequelize = require('sequelize'); // Import de Sequelize
+const Sequelize = require("sequelize"); // Import de Sequelize
 
 // Zone sequelize pour la base de données
 const sequelize = new Sequelize({
-	host: 'localhost',
-	dialect: 'sqlite',
-	logging: false,
-	// SQLite only
-	storage: './database/db.sqlite',
+    host: "localhost",
+    dialect: "sqlite",
+    logging: false,
+    // SQLite only
+    storage: "./database/db.sqlite",
 });
 
+// Initialisation de la table d'infos des artfights
 const table_artfight_info = sequelize.define("artfight", {
     id_guild: {
-        type: Sequelize.SMALLINT,
+        type: Sequelize.STRING,
         unique: true,
         primaryKey: true,
     },
@@ -21,7 +22,19 @@ const table_artfight_info = sequelize.define("artfight", {
     nom_equipe2: Sequelize.STRING,
     id_salon_equipe2: Sequelize.STRING,
     points_equipe2: Sequelize.INTEGER,
-    date: Sequelize.DATE
+    date: Sequelize.DATE,
+});
+
+// Initialisation de la table des users
+const table_user = sequelize.define("user", {
+    id_guild: {
+        type: Sequelize.STRING,
+        unique: true,
+        primaryKey: true,
+    },
+    id_user: Sequelize.STRING,
+    nom_equipe: Sequelize.STRING,
+    points: Sequelize.SMALLINT,
 });
 
 module.exports = { table_artfight_info };
