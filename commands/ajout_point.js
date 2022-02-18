5be57f14b55b4cb4e1c9f240ea0114be400d59cb
RisingSunLight <risingsunlight5776@gmail.com>
2022-02-18 19:34:07 +0100

    Ajout des vérifications si un artfight est en cours sur le serveur ou si le membre a rejoint une équipe ou pas

--- commands/ajout_point.js
+++ commands/ajout_point.js
@@ -5,6 +5,10 @@ const {
     MessageActionRow,
     MessageSelectMenu,
 } = require("discord.js");
+const {
+    table_artfight_info,
+    table_user,
+} = require("../database/database_gestion.js");
 
 // Crée la commande en faisant une nouvelle commande Slash
 module.exports = {
@@ -18,6 +22,36 @@ module.exports = {
      * @param {CommandInteraction} interaction
      */
     async execute(interaction) {
+        //* Récupération des infos
+        const id_guild = interaction.guildId;
+        const info_artfight = await table_artfight_info.findOne({
+            where: {
+                id_guild,
+            },
+            attributes: ["id_guild", "nom_equipe1", "nom_equipe2"],
+            raw: true,
+        });
+
+        //* Vérification si un artfight est en cours
+        if (!info_artfight)
+            return await interaction.reply({
+                content: "Il n'y a pas d'Artfight en cours sur ce serveur !",
+                ephemeral: true,
+            });
+
+        //* Vérification si le membre a déjà rejoins une équipe
+        if (
+            !(await table_user.findOne({
+                where: {
+                    id_guild,
+                    id_user: interaction.user.id,
+                },
+            }))
+        )
+            return await interaction.reply({
+                content: "Tu n'as pas rejoins d'équipe !",
+                ephemeral: true,
+            });
         const row = new MessageActionRow().addComponents(
             new MessageSelectMenu()
                 .setCustomId("question_points_1")
