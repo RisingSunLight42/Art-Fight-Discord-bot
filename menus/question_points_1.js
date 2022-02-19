68b7376e23ef51792ce8ed3b49a3d1536bfa41a0
RisingSunLight <risingsunlight5776@gmail.com>
2022-02-19 19:24:34 +0100

    Ajout de la récupération de l'enregistrement de l'user

--- menus/question_points_1.js
+++ menus/question_points_1.js
@@ -3,6 +3,13 @@ const { table_user } = require("../database/database_gestion.js"); // Import de 
 module.exports = {
     name: "question_points_1",
     async execute(client, interaction) {
-        //Exécution
+        const id_guild = interaction.guildId;
+        const id_user = interaction.user.id;
+        const user = await table_user.findOne({
+            where: {
+                id_guild,
+                id_user,
+            },
+        });
     },
 };
