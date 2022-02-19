884950b90efe53d5a5da3dc845cddfff6d8be0ed
RisingSunLight <risingsunlight5776@gmail.com>
2022-02-19 19:21:15 +0100

    Mise en place de la structure

---
+++ menus/question_points_1.js
@@ -0,0 +1,8 @@
+const { table_user } = require("../database/database_gestion.js"); // Import de la table pour l'artfight
+
+module.exports = {
+    name: "question_points_1",
+    async execute(client, interaction) {
+        //Exécution
+    },
+};
