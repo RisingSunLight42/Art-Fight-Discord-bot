e055f354d31c2a86325559f866c47f6de5e8ee02
RisingSunLight <risingsunlight5776@gmail.com>
2022-04-06 16:45:29 +0200

    Correction message de push des commandes + structure

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,27 +1,31 @@
-const fs = require('fs');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+const fs = require("fs");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const { clientId, guildId, token } = require("./config.json");
 
 const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));  // Récupère les fichiers .js des commandes se situant dans le dossier commands
+const commandFiles = fs
+    .readdirSync("./commands")
+    .filter((file) => file.endsWith(".js")); // Récupère les fichiers .js des commandes se situant dans le dossier commands
 
-for (const file of commandFiles) {                      // Parcours la liste de fichiers
-    const command = require(`./commands/${file}`);      // Récupère le fichier dans la variable command
-    commands.push(command.data.toJSON());               // Envoie la commande en format JSON
+for (const file of commandFiles) {
+    // Parcours la liste de fichiers
+    const command = require(`./commands/${file}`); // Récupère le fichier dans la variable command
+    commands.push(command.data.toJSON()); // Envoie la commande en format JSON
 }
 
-const rest = new REST({version: '9'}).setToken(token);  // Récupère l'API Discord
+const rest = new REST({ version: "9" }).setToken(token); // Récupère l'API Discord
 
 const deploy = async () => {
-    try {       // Envoie les nouvelles commandes à l'API pour les utiliser
-        await rest.put(Routes.applicationGuildCommands(clientId, guildId), { 
-            body: commands
+    try {
+        // Envoie les nouvelles commandes à l'API pour les utiliser
+        await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+            body: commands,
         });
-        console.log('Les commandes ont été enregistrées.');
+        console.log("🧪 Les commandes (/) locales ont été enregistrées.");
     } catch (error) {
         console.error(error);
     }
-}
+};
 
 module.exports = { deploy };
