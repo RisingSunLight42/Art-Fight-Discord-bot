352be9fc64adcb1d5c1dd4d5698c1dd82f73869b
RisingSunLight <risingsunlight5776@gmail.com>
2022-04-07 19:32:09 +0200

    Ajout du début de la fonction check_date et son appel

--- events/onready.js
+++ events/onready.js
@@ -1,5 +1,16 @@
 const { deploy } = require("../deploy-commands.js"); // Importe la fonction pour déployer les commandes
-const { sequelize } = require("../database/database_gestion.js"); // Import de la table pour l'artfight
+const {
+    sequelize,
+    table_artfight_info,
+    table_user,
+} = require("../database/database_gestion.js"); // Import de la table pour l'artfight
+
+const check_date = async () => {
+    // Fonction qui va check la date de chaque artfight pour voir si l'un d'entre-eux est terminé
+    const infos_guild_artfight = await table_artfight_info.findAll({
+        raw: true,
+    });
+};
 
 module.exports = {
     name: "ready",
@@ -13,5 +24,7 @@ module.exports = {
             activities: [{ name: "Je suis en préparation !" }],
             status: "dnd",
         });
+        check_date();
+        setInterval(check_date, 86400);
     },
 };
